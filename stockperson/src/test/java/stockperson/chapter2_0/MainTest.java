49b7ff7c2fce704b0dffd730ffcdf671fa1e5c91
Bahman Movaqar <bahman@bahmanm.com>
2025-01-05 16:17:05 -0800

    Implement chapter 2.0

---
+++ stockperson/src/test/java/stockperson/chapter2_0/MainTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2025 Bahman Movaqar
+ *
+ * This file is part of StockPerson-Java.
+ *
+ * StockPerson-Java is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * StockPerson-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StockPerson-Java. If not, see <https://www.gnu.org/licenses/>.
+ */
+package stockperson.chapter2_0;
+
+import static java.util.stream.Collectors.joining;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import stockperson.db.Db;
+
+class MainTest {
+
+  private final PrintStream originalSystemOut = System.out;
+  private ByteArrayOutputStream outputStream = null;
+  private PrintStream systemOut = null;
+
+  @AfterEach
+  public void restoreSystemOut() {
+    System.setOut(originalSystemOut);
+    systemOut.close();
+    systemOut = null;
+    try {
+      outputStream.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      outputStream = null;
+    }
+  }
+
+  @BeforeEach
+  public void setupSystemOut() {
+    outputStream = new ByteArrayOutputStream();
+    systemOut = new PrintStream(outputStream);
+    System.setOut(systemOut);
+    Db.clear();
+  }
+
+  @Test
+  void worksOk() throws IOException {
+    // GIVEN
+    var inputFilepath =
+        getClass().getResource("/stockperson-data-chapter2.0--set-01.csv").getPath();
+    var expectedOutputFile = getClass().getResource("/expected-output-chapter2.0.txt").getFile();
+    var expectedOutputFileReader = new FileReader(expectedOutputFile);
+    var expectedOutputReader = new BufferedReader(expectedOutputFileReader);
+    var expectedOutput = expectedOutputReader.lines().collect(joining("\n"));
+
+    // WHEN
+    Main.main(new String[] {inputFilepath});
+
+    // THEN
+    assertThat(outputStream.toString().trim()).isEqualTo(expectedOutput.trim());
+  }
+}
